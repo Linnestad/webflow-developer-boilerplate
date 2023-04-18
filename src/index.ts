d8e5821ede577636a5e48f847c1d8db5eaf39dbd
Jonas Linnestad <31269834+Linnestad@users.noreply.github.com>
2023-04-18 14:21:49 +0200

    added jquery to types in tsconfig

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,10 @@ window.Webflow.push(() => {
   const name = 'John Doe';
   greetUser(name);
 });
+
+/* eslint-disable no-console */
+console.log('Hello Webflow');
+
+$('#hello-world').on('click', function () {
+  console.log('hello world');
+});
