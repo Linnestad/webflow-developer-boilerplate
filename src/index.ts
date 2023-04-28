0bebddab4d06f73299672bceba4ee71e63f7c03b
Linnestad <31269834+Linnestad@users.noreply.github.com>
2023-04-28 11:04:12 +0200

    Included Webflow Editor

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-import { greetUser } from '$utils/greet';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+export {};
+declare global {
+  interface Window {
+    WebflowEditor: unknown;
+  }
+}
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
-});
-
-$('#button').on('click', function () {
-  console.log('hello world');
+  if (!window.WebflowEditor) {
+    console.log('Put code in here to disable these running in the webflow content editor');
+  } else {
+    console.log('Editor');
+  }
 });
