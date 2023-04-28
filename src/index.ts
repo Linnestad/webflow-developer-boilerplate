34ce8984bb9f814a7fb77ae4aff707eec40a1f7c
Linnestad <31269834+Linnestad@users.noreply.github.com>
2023-04-28 15:34:25 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable no-console */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 
 export {};
 declare global {
